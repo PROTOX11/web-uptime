ca30ebb769465135a0e17331249bcef27474c98a
Prakash kumar <prakashkr2894@gmail.com>
2025-04-15 16:15:41 +0530

    bun installed on every folder and backend cateogorized

---
+++ apps/api/index.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import { authMiddleware } from './middleware';
+import { prismaClient } from "db/client";
+
+const app = express();
+app.use(express.json());
+
+app.post("/api/v1/website", authMiddleware, (req, res) => {
+    const userId = req.userId!;
+    const {url} = req.body;
+
+    await prismaClient.website.create({
+        data: {
+            userId,
+            url
+        }
+
+    })
+})
+app.get("/api/v1/website/status", authMiddleware, (req, res) => {
+
+})
+app.get("/api/v1/websites", authMiddleware, (req, res) => {
+
+})
+app.delete("/api/v1/website/", authMiddleware, (req, res) => {
+
+})
+
+app.listen(3000);
